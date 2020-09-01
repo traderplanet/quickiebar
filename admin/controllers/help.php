60c42eee41cd20682c17796f47cd513b137e3104
Terre Porter <terre.porter@gmail.com>
2020-09-01 12:09:54 -0400

    Update global function to check user sub status
    - minor code cleanup

--- admin/controllers/help.php
+++ admin/controllers/help.php
@@ -21,7 +21,7 @@ class qb_help{
 	
 	//echo out the settings view (html file) file when loading the bars admin page
 	function html(){
-		readfile(QB_PLUGIN_PATH . 'admin/views/help.html');
+		echo file_get_contents(QB_PLUGIN_PATH . 'admin/views/help.html');
 		
 		//enqueue scripts for this view
 		$this->enqueue_scripts_for_view();
